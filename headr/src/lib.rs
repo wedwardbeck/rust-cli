094fcedab194d0a4a097a2f2a749b9b34ba31f8d
Edward Beck <wedwardbeck@gmail.com>
2023-01-15 20:10:58 -0600

    Done Ch3/4

---
+++ headr/src/lib.rs
@@ -0,0 +1,11 @@
+use clap::{Arg, App};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
